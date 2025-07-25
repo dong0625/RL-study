c0d1399fb83c8d3d28f7a5b3e734f9ee98b81073
dong0625 <dong0625@korea.ac.kr>
2025-07-25 16:50:07 +0900

    Update base line

--- model/DQN.py
+++ model/DQN.py
@@ -1,8 +1,10 @@
 '''
 Paper : Playing Atari with Deep Reinforcement Learning
 
-The model is a convolutional neural network,
-trained with a variant of Q-learning
-whose input is raw pixels 
-and whose output is a value function estimating future rewards.
+Abstract
+    The model is a convolutional neural network,
+    trained with a variant of Q-learning
+    whose input is raw pixels and
+    whose output is a value function estimating future rewards.
 '''
+
